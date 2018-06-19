a75f81e843cd6a7dc22ccfd487d3c95d6edb3c71
Tom Shen <yifengshen2004@gmail.com>
2018-06-19 10:38:42 +0800

    Added Mac Target

---
+++ EA Center Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EA Center Mac
+//
+//  Created by Tom Shen on 2018/6/19.
+//  Copyright © 2018 Tom Shen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EA Center Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EA Center Mac
+//
+//  Created by Tom Shen on 2018/6/19.
+//  Copyright © 2018 Tom Shen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
