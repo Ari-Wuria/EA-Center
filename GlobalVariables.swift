973fa97075a52ce46ef2e5840a045fd39be021d6
Tom Shen <tomshen2004@163.com>
2018-06-25 17:54:24 +0800

    Changed Manage EA into a tab view controller

---
+++ GlobalVariables.swift
@@ -0,0 +1,15 @@
+//
+//  GlobalVariables.swift
+//  EA Center Mac
+//
+//  Created by Tom Shen on 2018/6/25.
+//  Copyright © 2018 Tom Shen. All rights reserved.
+//
+
+// Used to define global variables
+
+import Foundation
+
+var MainServerAddress = "http://jerrytomlouise.asuscomm.com:81/eacenter"
+
+
